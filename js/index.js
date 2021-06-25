a520d49ad21c7e5701f26628479b9389ac8cf9eb
Wesley Westelley <wesleywestelley@gmail.com>
2021-06-25 16:09:58 -0300

    simple calculations working

--- js/index.js
+++ js/index.js
@@ -5,17 +5,18 @@ const $output = document.querySelector('.output')
 const $clear = document.querySelector('[data-js="clear"]')
 const $clearAll = document.querySelector('[data-js="clearAll"]')
 const $equal = document.querySelector('[data-js="equal"]')
+const $porcentage = document.querySelector('[data-js="porcentage"]')
 let equation
 let result = 0
 
 $clear.addEventListener('click', clear)
 $clearAll.addEventListener('click', clearAll)
 $equal.addEventListener('click', calculate)
+$porcentage.addEventListener('click', porcentage)
 
 $number.forEach((element) => {
     element.addEventListener('click', () => {
         if ($output.innerText.length < 8) $output.innerText += element.textContent + ' '
-
     })
 })
 
@@ -25,7 +26,6 @@ $operators.forEach((element) => {
             $input.innerText += ' ' + $output.textContent + ' ' + element.textContent + ' '
             $output.innerText = ''
         }
-
     })
 })
 
@@ -37,39 +37,60 @@ function calculate() {
         setTimeout(() => {
             equation = $input.innerText
             equation = equation.split(' ')
-
-            for (let i = 0; i < equation.length; i++) {
+            for (let i = 0; i <= equation.length; i++) {
 
                 switch (equation[i]) {
                     case '+':
-                        result += equation[i - 1] ? Number(equation[i - 1]) + Number(equation[i + 1]) : result += Number(equation[i + 1])
-                        equation.shift()
-                        equation.shift()
-                        equation.shift()
-                        equation.unshift()
+                        result = Number(equation[i - 1])
+                        result += Number(equation[i + 1])
+                        equationManipulation()
                         i = 0
                         $output.innerText = result
                         break
                     case '×':
-                        result += Number(equation[i - 1]) * Number(equation[i + 1])
+                        result = Number(equation[i - 1])
+                        result *= Number(equation[i + 1])
+                        equationManipulation()
+                        i = 0
                         $output.innerText = result
                         break
                     case '÷':
-                        result += Number(equation[i - 1]) / Number(equation[i + 1])
+                        result = Number(equation[i - 1])
+                        result /= Number(equation[i + 1])
+                        equationManipulation()
+                        i = 0
                         $output.innerText = result
                         break
                     case '-':
-                        result += Number(equation[i - 1]) - Number(equation[i + 1])
-                        $output.innerText = result
-                        break
-                    case '%':
-                        result += Number(equation[i - 1]) % Number(equation[i + 1])
+                        result = Number(equation[i - 1])
+                        result -= Number(equation[i + 1])
+                        equationManipulation()
+                        i = 0
                         $output.innerText = result
                         break
                 }
             }
         }, 1)
     }
+}
+
+function equationManipulation() {
+    equation.shift()
+    equation.shift()
+    equation.shift()
+    equation.unshift(result)
+
+    for(index in equation) {
+        if(equation[index] == '=') {
+            equation.splice(index,1)
+        }
+    }
+}
+
+function porcentage() {
+    console.log('Olá')
+    let porcentage = $output.innerText
+    $output.innerText = Number(porcentage/100)
 }
 
 function clear() {
